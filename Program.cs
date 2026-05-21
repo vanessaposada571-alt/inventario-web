e8320b30e36a41e923855883a898696354fb9534
vanessaposada571 <vanessa.posada571@pascualbravo.edu.co>
2026-05-21 11:46:18 -0500

    correciones web

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication24_02.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container for Razor Pages and MVC controllers
